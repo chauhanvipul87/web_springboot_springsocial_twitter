f8035800a78f34d0b2a0b6e9775f6c1f1a65f9e6
vipul Chauhan <test.jpatel1@gmail.com>
2016-05-10 13:56:49 +0530

    initial commit

---
+++ SpringSocialTwitter/src/main/java/com/iana/SpringSocialTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.iana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialTwitterApplication.class, args);
+	}
+}
